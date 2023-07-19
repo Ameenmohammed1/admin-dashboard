05e772f801057ce963445e5b4f4781f46252eb63
futurebeets <futurebeets1@gmail.com>
2023-07-19 12:56:20 -0400

    home page created

--- src/AdminComponects/Home/Home.jsx
+++ src/AdminComponects/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Home.css'
+import UserTable from '../DataTable/User'
 function Home() {
   return (
     <div className='HomeParent'>
         <div className="head">
             <p>Profile</p>
             <button>Creat</button>
-        </div>
+            </div>
+            <div className="dataTabelParent">
+            <UserTable/>
+            </div>
+            
+       
     </div>
   )
 }
